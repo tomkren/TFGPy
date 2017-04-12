88f2d6bdeb9da43de3ebcb253c15e2a72cbb97ad
Joe Moudrik <j.moudrik@gmail.com>
2017-04-12 18:36:49 +0200

    generator_static: debug wip

--- generator_static.py
+++ generator_static.py
@@ -19,10 +19,10 @@ def ts_1(gamma, typ, n):
 
 
 def ts_ij(gamma, i, j, typ, n):
-    print("ts_ij\t", i, j, n, "'%s'"%typ)
+    print("ENTER ts_ij\t", i, j, n, "'%s'"%typ)
 
     nase = False
-    if str(typ) == '(.0 -> ((P A (P A A)) -> (P A (P A A))))':
+    if i==2 and j == 1 and n==0 and str(typ) == '((P A (P A A)) -> (P A (P A A)))':
         print("#"*20)
         nase = True
     ret = []
@@ -34,9 +34,11 @@ def ts_ij(gamma, i, j, typ, n):
         O+=1
         print("res_f", O, I)
         typ_x = res_f.sub(alpha)
+        print(res_f.sub)
         for res_x in ts(gamma, j, typ_x, res_f.n):
             I+=1
             print("res_x", O,I)
+            print(res_x.sub)
             sigma_fx = sub.dot(res_x.sub, res_f.sub).restrict(typ)
             tree_f = res_f.tree.apply_sub(res_x.sub)
             tree_fx = App(tree_f, res_x.tree, sigma_fx(typ))
@@ -44,6 +46,7 @@ def ts_ij(gamma, i, j, typ, n):
                 assert False
             ret.append(TsRes(tree_fx, sigma_fx, res_x.n))
 
+    print("LEAVE ts_ij\t", i, j, n, "'%s'"%typ)
     return ret
 
 
