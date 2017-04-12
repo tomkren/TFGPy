15d1cad8f8390d3c1a9ee1aad1aa7e74e4415074
Joe Moudrik <j.moudrik@gmail.com>
2017-04-12 14:48:08 +0200

    app_tree: added __str__()

--- app_tree.py
+++ app_tree.py
@@ -8,8 +8,14 @@ class App(AppTree):
         self.arg = arg
         self.typ = typ
 
+    def __str__(self):
+        return "(%s %s)"%(self.fun, self.arg)
+
 
 class Leaf(AppTree):
     def __init__(self, sym, typ):
         self.sym = sym
         self.typ = typ
+
+    def __str__(self):
+        return str(self.sym)
