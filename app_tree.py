b798f3bc8eeb4a8e2475a04a2569b2c8a5074413
Joe Moudrik <j.moudrik@gmail.com>
2017-04-12 14:50:11 +0200

    app_tree: added __repr__()

--- app_tree.py
+++ app_tree.py
@@ -8,14 +8,20 @@ class App(AppTree):
         self.arg = arg
         self.typ = typ
 
+    def __repr__(self):
+        return "App(%s, %s, %s)" % (repr(self.fun), repr(self.arg), repr(self.typ))
+
     def __str__(self):
-        return "(%s %s)"%(self.fun, self.arg)
+        return "(%s %s)" % (self.fun, self.arg)
 
 
 class Leaf(AppTree):
     def __init__(self, sym, typ):
         self.sym = sym
         self.typ = typ
+
+    def __repr__(self):
+        return "Leaf(%s, %s)" % (repr(self.sym), repr(self.typ))
 
     def __str__(self):
         return str(self.sym)
