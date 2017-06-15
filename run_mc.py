e5914fc0399bacc3a4d47543e39eeb250a998e84
Joe Moudrik <j.moudrik@gmail.com>
2017-06-15 13:00:21 +0200

    run_mc: remove functionality that is in run_experiment.py

--- run_mc.py
+++ run_mc.py
@@ -10,48 +10,8 @@ from tree_stats import TreeStats
 from utils import experiment_eval
 
 if __name__ == "__main__":
-    # make_env = make_env
-    make_env = lambda: make_env_stack(20)
-
-    if not False:
-        # Nested MC Search
-        def one_iteration(worker_env):
-            env = make_env()
-            evals_before = env.count_evals()
-            assert not evals_before
-            time_before = time.time()
-            # root = ChooseKTNode(UnfinishedLeaf(), 5)
-            root = StackNode([])
-            indiv = nested_mc_search(root,
-                                     max_level=2,
-                                     fitness=env.fitness,
-                                     finish=env.finish,
-                                     is_finished=env.is_finished,
-                                     successors=env.successors,
-                                     advance=env.advance,
-                                     early_end_test=env.early_end_test)
-            return env.fitness(indiv), env.count_evals() - evals_before, time.time() - time_before
-
-
-        experiment_eval(one_iteration, repeat=1000, processes=4, make_env=lambda: None)
-
-    if False:
-        # MCTS
-        def one_iteration(worker_env):
-            evals_before = env.count_evals()
-            time_before = time.time()
-            # root = MCTNode(ChooseKTNode(UnfinishedLeaf(), 5))
-            root = MCTNode(StackNode([]))
-            mct_search(root, expand_visits=8, num_steps=1000,
-                       fitness=env.fitness,
-                       finish=env.finish,
-                       is_finished=env.is_finished,
-                       successors=env.successors,
-                       early_end_test=env.early_end_test)
-            return root.best_score, env.count_evals() - evals_before, time.time() - time_before
-
-
-        experiment_eval(one_iteration, repeat=10, processes=4, make_env=make_env)
+    make_env = make_env_app_tree
+    #make_env = lambda: make_env_stack(20)
 
     if False:
         # MCTS - one run
