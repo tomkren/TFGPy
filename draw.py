df6e5f7b7e77e43124c26d7feb63159ba207c961
Tomáš Křen <tomkren@gmail.com>
2018-04-14 19:13:24 +0200

    draw.py: basic dataset generator

--- draw.py
+++ draw.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageDraw
 from collections import OrderedDict
 from time import time
+import os
 import imagehash
 
 from parsers import parse_typ, parse_ctx, fun_typ
@@ -23,24 +24,35 @@ Colors = {
 }
 
 
-def main():
-
+def make_hand_crafted_examples(img_size):
     code_001 = h(v(G, h(v(G, h(v(G, h(v(G, B), B)), B)), B)), B)
     code_002 = q(Re, Gr, Bl, q(Re, Gr, Bl, q(Re, Gr, Bl, q(Re, Gr, Bl, q(Re, Gr, Bl, q(Re, Gr, Bl, q(Re, Gr, Bl, q(Re, Gr, Bl, G))))))))
     elephant = q(G, q(q(W, W, q(G, G, B, G), W), W, G, W), q(W, G, q(G, G, G, q(W, W, G, W)), q(G, G, W, G)),
                 q(q(q(W, q(W, W, G, q(G, W, q(q(W, W, W, G), G, W, W), W)), W, G), G, W, W), W, q(W, W, q(W, W, G, W), W), W))
 
-    img_size = (512, 512)
     codes = [code_001, code_002, elephant]
 
+    dir_path = 'imgs/handmade/'
+    ensure_dir(dir_path)
+    filename_pat = dir_path + '%03d.png'
+
     for i, code in enumerate(codes):
-        render_to_file('imgs/handmade/%03d.png' % (i+1), img_size, code)
-
-    max_k = 9  # 2348
-    test_domain(make_domain, max_k, img_size)
+        render_to_file(filename_pat % (i+1), img_size, code)
 
 
-def make_domain():
+def main():
+
+    path = 'imgs/gen'
+    img_size = (32, 32)
+    domain_maker = make_family1
+    max_tree_size = 11  # 7 -> ~258, 9 -> ~2348, 11 -> ~?
+
+    make_hand_crafted_examples(img_size)
+
+    generate_dataset(path, domain_maker, max_tree_size, img_size)
+
+
+def make_family1():
     t_img = parse_typ('I')  # simple Image type
 
     t_op2 = fun_typ((t_img, t_img), t_img)  # Simple binary operation
@@ -58,17 +70,36 @@ def make_domain():
     return goal, gamma
 
 
-def test_domain(domain_maker, max_k, img_size):
+def generate_dataset(path, domain_maker, max_tree_size, img_size):
     start_time = time()
+
+    if not path.endswith('/'):
+        path += '/'
+
+    imgs_path = path + 'imgs/'
+
+    ensure_dir(imgs_path)
+
+    img_filename_pat = imgs_path + '%05d.png'
+    stats_filename = path + 'stats.txt'
+
+    jsons_filename = path + 'jsons.txt'
+    prefix_filename = path + 'prefix.txt'
+    roots_filename = path + 'roots.txt'
+
+    open(jsons_filename, 'w').close()
+    open(prefix_filename, 'w').close()
+    open(roots_filename, 'w').close()
 
     goal, gamma = domain_maker()
     gen = Generator(gamma)
 
     img_hashes = {}
+    stats_for_sizes = []
 
     i = 1
 
-    for k in range(1, max_k + 1):
+    for k in range(1, max_tree_size + 1):
         num = gen.get_num(k, goal)
 
         print('size =', k, "-> num =", num)
@@ -78,6 +109,7 @@ def test_domain(domain_maker, max_k, img_size):
             # example_tree = gen.gen_one(k, goal)
 
             all_trees = ts(gamma, k, goal, 0)
+            new_for_this_size = 0
 
             for tree_data in all_trees:
 
@@ -91,18 +123,51 @@ def test_domain(domain_maker, max_k, img_size):
                 if img_hash not in img_hashes:
                     img_hashes[img_hash] = img_code
                     root_sym = root_symbol(img_code)
-                    im.save('imgs/gen/%04d-%s.png' % (i, root_sym), 'PNG')
+                    im.save(img_filename_pat % i, 'PNG')
+
+                    prefix_code = to_prefix_notation(img_code)
+
+                    append_line(roots_filename, root_sym)
+                    append_line(prefix_filename, prefix_code)
+                    append_line(jsons_filename, str(img_code))
 
                     print('\t', i)
                     print('\t\t example_tree   =', tree)
-                    print('\t\t example s-expr =', img_code)
+                    print('\t\t s-expr =', img_code)
+                    print('\t\t prefix =', prefix_code)
                     print('\t\t img_hash =', img_hash)
 
                     i += 1
+                    new_for_this_size += 1
 
+            stats_for_sizes.append((k, num, new_for_this_size))
+
+    num_generated_trees = i - 1
     delta_time = time() - start_time
-    print()
-    print('test_domain(..) took %.2f seconds.' % delta_time)
+
+    stats = 'Num Generated Images: %d\n' % num_generated_trees
+    stats += 'Generating Time: %.2f s\n' % delta_time
+    stats += 'Stats fo Sizes:\n'
+
+    stats += '%-10s%-20s%s\n' % ('Tree size', 'Num of all trees', 'New trees')
+    for o in stats_for_sizes:
+        stats += '%-10d%-20d%d\n' % o
+
+    with open(stats_filename, 'w') as stats_file:
+        stats_file.write(stats)
+
+    print('\n'+stats)
+
+
+def append_line(filename, line):
+    with open(filename, 'a') as f:
+        f.write("%s\n" % line)
+
+
+def ensure_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 def root_symbol(code):
@@ -110,6 +175,17 @@ def root_symbol(code):
         return root_symbol(code[0])
     else:
         return code
+
+
+def to_prefix_notation(code):
+    return ' '.join(to_prefix_json(code))
+
+
+def to_prefix_json(code):
+    if isinstance(code, list):
+        return sum([to_prefix_json(arg) for arg in code], [])
+    else:
+        return [code]
 
 
 def render(code, zoom, draw):
