c52974c3b57123fa6f41c8d54e0c139992d8f5c1
Tomáš Křen <tomkren@gmail.com>
2018-04-15 11:28:37 +0200

    draw.py: preparations for more sophisticated sampling

--- draw.py
+++ draw.py
@@ -1,12 +1,181 @@
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw  # Using https://pillow.readthedocs.io
 from collections import OrderedDict
 from time import time
 import os
-import imagehash
+import imagehash  # Using https://pypi.python.org/pypi/ImageHash
 
 from parsers import parse_typ, parse_ctx, fun_typ
 from generator import Generator
 from generator_static import ts
+
+
+def main():
+
+    make_hand_crafted_examples((512, 512))  # Run quick example generation ..
+
+    path = 'imgs/gen'
+    dim_size = 32
+    img_size = (dim_size, dim_size)
+    hash_size = 32
+    highfreq_factor = 4
+
+    gen_opts = {
+        'exhaustive_generating_limit': 10000,  # 250000,
+        'sample_method': {
+            'name': 'simple_sampling',
+            'num_attempts': 100000
+        }
+    }
+
+    domain_maker = make_family_1
+    max_tree_size = 13  # 7 -> ~264, 9 -> ~2602, 11 -> ~28148
+
+    generate_dataset(path, domain_maker, max_tree_size, gen_opts, img_size, hash_size, highfreq_factor)
+
+
+def generate_dataset(path, domain_maker, max_tree_size, gen_opts, img_size, hash_size=8, highfreq_factor=4):
+    start_time = time()
+
+    exhaustive_generating_limit = gen_opts['exhaustive_generating_limit']
+
+    if not path.endswith('/'):
+        path += '/'
+
+    imgs_path = path + 'imgs/'
+
+    ensure_dir(imgs_path)
+
+    # img_filename_pat = imgs_path + '%07d.png'
+    stats_filename = path + 'stats.txt'
+
+    # jsons_filename = path + 'jsons.txt'
+    # prefix_filename = path + 'prefix.txt'
+    # roots_filename = path + 'roots.txt'
+
+    paths = {
+        'img_pattern': imgs_path + '%08d.png',
+        'jsons': path + 'jsons.txt',
+        'prefix': path + 'prefix.txt',
+        'roots': path + 'roots.txt'
+    }
+
+    open(paths['jsons'], 'w').close()
+    open(paths['prefix'], 'w').close()
+    open(paths['roots'], 'w').close()
+
+    goal, gamma = domain_maker()
+    gen = Generator(gamma)
+
+    img_hashes = {}
+    stats_for_sizes = []
+
+    i = 1
+
+    for k in range(1, max_tree_size + 1):
+        num = gen.get_num(k, goal)
+
+        print('size =', k, "-> num =", num)
+
+        if num > 0:
+
+            new_for_this_size = 0
+
+            if num < exhaustive_generating_limit:
+
+                for tree_data in ts(gamma, k, goal, 0):
+
+                    tree = tree_data.tree
+
+                    img_code = tree.to_sexpr_json()
+
+                    im = render_to_img(img_size, img_code)
+                    img_hash = imagehash.phash(im, hash_size, highfreq_factor)
+                    # print('\t img_hash =', img_hash)
+
+                    if img_hash not in img_hashes:
+                        img_hashes[img_hash] = k   # img_code
+
+                        save_generated_tree_data(i, im, img_code, tree, paths)
+
+                        # im.save(img_filename_pat % i, 'PNG')
+
+                        # root_sym = root_symbol(img_code)
+                        # prefix_code = to_prefix_notation(img_code)
+
+                        # append_line(roots_filename, root_sym)
+                        # append_line(prefix_filename, prefix_code)
+                        # append_line(jsons_filename, str(img_code))
+
+                        # print('\t', i)
+                        # print('\t\t example_tree   =', tree)
+                        # print('\t\t s-expr =', img_code)
+                        # print('\t\t prefix =', prefix_code)
+                        # print('\t\t img_hash =', img_hash)
+
+                        i += 1
+                        new_for_this_size += 1
+            else:
+
+                # example_tree = gen.gen_one(k, goal)
+
+                pass
+
+            new_to_all_percent = (100.0 * new_for_this_size) / num
+            stats_for_sizes.append((k, num, new_for_this_size, new_to_all_percent))
+
+    num_generated_trees = i - 1
+    delta_time = time() - start_time
+
+    stats = 'Num Generated Images: %d\n' % num_generated_trees
+    stats += 'Generating Time: %.2f s\n' % delta_time
+    stats += 'Image size: %d×%d\n' % img_size
+    stats += 'pHash params (size, highfreq_factor): %d, %d\n' % (hash_size, highfreq_factor)
+
+    stats += 'Stats for Sizes:\n'
+    stats += '%-15s%-20s%-20s%s\n' % ('Tree size', 'Num of all trees', 'New trees', 'New/All %')
+    for o in stats_for_sizes:
+        stats += '%-15d%-20d%-20d%.2f\n' % o
+
+    with open(stats_filename, 'w') as stats_file:
+        stats_file.write(stats)
+
+    print('\n'+stats)
+
+
+def save_generated_tree_data(i, im, img_code, tree, paths):
+
+    im.save(paths['img_pattern'] % i, 'PNG')
+
+    root_sym = root_symbol(img_code)
+    prefix_code = to_prefix_notation(img_code)
+
+    append_line(paths['roots'], root_sym)
+    append_line(paths['prefix'], prefix_code)
+    append_line(paths['jsons'], str(img_code))
+
+    print('%-8d->' % i, paths['img_pattern'] % i)
+    print('\t\ttree =', tree)
+    print('\t\ts-expr =', img_code)
+    print('\t\tprefix =', prefix_code)
+
+
+def make_family_1():
+    t_img = parse_typ('I')  # simple Image type
+
+    t_op2 = fun_typ((t_img, t_img), t_img)  # Simple binary operation
+    t_op4 = fun_typ((t_img, t_img, t_img, t_img), t_img)  # Simple tetra operation
+
+    goal = t_img
+    gamma = parse_ctx(OrderedDict([
+        (H, t_op2),
+        (V, t_op2),
+        (Q, t_op4),
+        (W, t_img),
+        (B, t_img)
+    ]))
+
+    return goal, gamma
+
 
 H, V, Q, C = 'h', 'v', 'q', 'c'
 
@@ -38,125 +207,6 @@ def make_hand_crafted_examples(img_size):
 
     for i, code in enumerate(codes):
         render_to_file(filename_pat % (i+1), img_size, code)
-
-
-def main():
-
-    path = 'imgs/gen'
-    img_size = (32, 32)
-    domain_maker = make_family1
-    max_tree_size = 11  # 7 -> ~258, 9 -> ~2348, 11 -> ~?
-
-    make_hand_crafted_examples(img_size)
-
-    generate_dataset(path, domain_maker, max_tree_size, img_size)
-
-
-def make_family1():
-    t_img = parse_typ('I')  # simple Image type
-
-    t_op2 = fun_typ((t_img, t_img), t_img)  # Simple binary operation
-    t_op4 = fun_typ((t_img, t_img, t_img, t_img), t_img)  # Simple tetra operation
-
-    goal = t_img
-    gamma = parse_ctx(OrderedDict([
-        (H, t_op2),
-        (V, t_op2),
-        (Q, t_op4),
-        (W, t_img),
-        (B, t_img)
-    ]))
-
-    return goal, gamma
-
-
-def generate_dataset(path, domain_maker, max_tree_size, img_size):
-    start_time = time()
-
-    if not path.endswith('/'):
-        path += '/'
-
-    imgs_path = path + 'imgs/'
-
-    ensure_dir(imgs_path)
-
-    img_filename_pat = imgs_path + '%05d.png'
-    stats_filename = path + 'stats.txt'
-
-    jsons_filename = path + 'jsons.txt'
-    prefix_filename = path + 'prefix.txt'
-    roots_filename = path + 'roots.txt'
-
-    open(jsons_filename, 'w').close()
-    open(prefix_filename, 'w').close()
-    open(roots_filename, 'w').close()
-
-    goal, gamma = domain_maker()
-    gen = Generator(gamma)
-
-    img_hashes = {}
-    stats_for_sizes = []
-
-    i = 1
-
-    for k in range(1, max_tree_size + 1):
-        num = gen.get_num(k, goal)
-
-        print('size =', k, "-> num =", num)
-
-        if num > 0:
-
-            # example_tree = gen.gen_one(k, goal)
-
-            all_trees = ts(gamma, k, goal, 0)
-            new_for_this_size = 0
-
-            for tree_data in all_trees:
-
-                tree = tree_data.tree
-
-                img_code = tree.to_sexpr_json()
-
-                im = render_to_img(img_size, img_code)
-                img_hash = imagehash.phash(im)
-
-                if img_hash not in img_hashes:
-                    img_hashes[img_hash] = img_code
-                    root_sym = root_symbol(img_code)
-                    im.save(img_filename_pat % i, 'PNG')
-
-                    prefix_code = to_prefix_notation(img_code)
-
-                    append_line(roots_filename, root_sym)
-                    append_line(prefix_filename, prefix_code)
-                    append_line(jsons_filename, str(img_code))
-
-                    print('\t', i)
-                    print('\t\t example_tree   =', tree)
-                    print('\t\t s-expr =', img_code)
-                    print('\t\t prefix =', prefix_code)
-                    print('\t\t img_hash =', img_hash)
-
-                    i += 1
-                    new_for_this_size += 1
-
-            stats_for_sizes.append((k, num, new_for_this_size))
-
-    num_generated_trees = i - 1
-    delta_time = time() - start_time
-
-    stats = 'Num Generated Images: %d\n' % num_generated_trees
-    stats += 'Generating Time: %.2f s\n' % delta_time
-    stats += 'Stats fo Sizes:\n'
-
-    stats += '%-10s%-20s%s\n' % ('Tree size', 'Num of all trees', 'New trees')
-    for o in stats_for_sizes:
-        stats += '%-10d%-20d%d\n' % o
-
-    with open(stats_filename, 'w') as stats_file:
-        stats_file.write(stats)
-
-    print('\n'+stats)
 
 
 def append_line(filename, line):
--- imgs/readme.md
+++ imgs/readme.md
@@ -1,5 +1,154 @@
-# imgs
+# i2c imgs #
 
 This directory is a place where the generated images are saved.
 Currently, the script generating them is the `draw.py` script.
 These images are intended for neural network experiments.
+
+
+## Notes ##
+
+- idea : concat more hashes ...
+
+
+
+
+
+
+## Sampling generating possibilities ##
+
+- simple_sampling: sample fixed number of times, without consideration of number of fails 
+- try to generate fix number of trees, with som limits that may fail due to too much attempts
+
+
+
+
+## Experiment: Influence of options on num gen trees
+
+
+### 32, hash_size=8 
+
+Num Generated Images: 242
+Generating Time: 6.80 s
+Stats fo Sizes:
+Tree size Num of all trees    New trees
+1         2                   2
+3         8                   4
+5         80                  26
+7         1024                210
+
+Num Generated Images: 22880
+Generating Time: 1666.82 s
+Size Stats:
+size      num                 new
+1         2                   2
+3         8                   4
+5         80                  26
+7         1024                210
+9         14848               2054
+11        231936              20584
+
+
+
+### 32, hash_size=16 
+
+Num Generated Images: 264
+Generating Time: 7.21 s
+Image size: 32×32
+Stats fo Sizes:
+Tree size      Num of all trees    New trees
+1              2                   2
+3              8                   4
+5              80                  26
+7              1024                232
+
+Num Generated Images: 2602
+Generating Time: 136.94 s (mozna nepresne, bezelo toho vic zrovna)
+Image size: 32×32
+pHash size: 16
+Stats for Sizes:
+Tree size      Num of all trees    New trees
+1              2                   2
+3              8                   4
+5              80                  26
+7              1024                232
+9              14848               2338
+
+### 32, hash_size=32, highfreq_factor=4
+
+Num Generated Images: 2612
+Generating Time: 136.40 s
+Image size: 32×32
+pHash size: 32
+Stats for Sizes:
+Tree size      Num of all trees    New trees
+1              2                   2
+3              8                   4
+5              80                  26
+7              1024                232
+9              14848               2348
+
+Num Generated Images: 28148
+Generating Time: 1621.01 s
+Image size: 32×32
+pHash size & highfreq_factor: 32, 4
+Stats for Sizes:
+Tree size      Num of all trees    New trees           New/All %
+1              2                   2                   100.00
+3              8                   4                   50.00
+5              80                  26                  32.50
+7              1024                232                 22.66
+9              14848               2348                15.81
+11             231936              25536               11.01
+
+
+### 32, 32, 8
+
+Num Generated Images: 2608
+Generating Time: 163.71 s
+Image size: 32×32
+pHash size & highfreq_factor: 32, 8
+Stats for Sizes:
+Tree size      Num of all trees    New trees
+1              2                   2
+3              8                   4
+5              80                  26
+7              1024                232
+9              14848               2344
+
+
+### 64, hash_size=8 
+
+Num Generated Images: 258
+Generating Time: 7.25 s
+Image size: 64×64
+Stats fo Sizes:
+Tree size      Num of all trees    New trees
+1              2                   2
+3              8                   4
+5              80                  26
+7              1024                226
+
+### 512, hash_size=16
+
+Num Generated Images: 264
+Generating Time: 11.57 s
+Image size: 512×512
+pHash size: 16
+Stats for Sizes:
+Tree size      Num of all trees    New trees
+1              2                   2
+3              8                   4
+5              80                  26
+7              1024                232
+
+### 1024, hash_size=8 
+
+Num Generated Images: 258
+Generating Time: 27.85 s
+Image size: 1024×1024
+Stats fo Sizes:
+Tree size      Num of all trees    New trees
+1              2                   2
+3              8                   4
+5              80                  26
+7              1024                226
