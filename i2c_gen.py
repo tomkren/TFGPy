7a1410efb71b5e4074383e5ffbde226caf1f89cc
Tomáš Křen <tomkren@gmail.com>
2018-09-17 13:08:43 +0200

    i2c: nm install script (in progress), i2c cmd line interface stub

---
+++ i2c_gen.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env pypy3
+import argparse
+
+GO_small = 'small'
+GO_full = 'full'
+GO_requested_128 = 'requested_128'
+GO_003similar = '003similar'
+
+GO_ALL = [GO_small, GO_full, GO_requested_128, GO_003similar]
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--domain', type=str, choices=GO_ALL, default=GO_small)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    print(args)
+
+
+if __name__ == "__main__":
+    main()
---
+++ imgs/nm/instal-nm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# It is not a good idea to run this scipt directly, since it is probable that it will fail for some reason,
+# and it won't react to this fail. Instead, it is recommanded to manually execute each non-echo line.
+
+echo Until this script is properly tested, we exit right after the start to prevent any damage
+exit
+
+echo Go to home dir ...
+cd ~
+
+echo this is probably already done, but for the record ...
+git clone https://github.com/tomkren/TFGPy.git
+
+
+echo Python version is ...
+python3 -V
+
+echo Creating venv for neural monkey ...
+python3 -m venv nm
+
+echo Clone neural monkey repo ...
+git clone https://github.com/ufal/neuralmonkey
+
+echo Install distribute thingy, a problematic prerequisity not in requirements.txt ...
+easy_install distribute
+
+echo Install numpy separetly, for some reason this is neededed oterwise requirements.txt install fails ...
+pip install numpy
+
+echo Go to neural monkey dir ...
+cd neuralmonkey/
+
+echo Install the rest of neural monkey prerequisities ...
+pip install --upgrade -r requirements.txt
+
+echo Back to the home dir ...
+cd ..
+
+
+
+
